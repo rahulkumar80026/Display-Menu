5b35ccb4c814460f87977640393e447f483cdb81
Rahul kumar <92114290+rahulkumar80026@users.noreply.github.com>
2025-09-19 17:35:15 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://display-menu-1.onrender.com/api", // ✅ change this to your backend URL
+  baseURL: "https://display-menu-1.onrender.com/", // ✅ change this to your backend URL
 });
 
 export default API;
